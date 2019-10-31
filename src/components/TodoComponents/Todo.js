d2d6f7441dcc9c0d5a8a3aa4a23a1166a6f24c09
Nerissa Taylor <taylornerissa@hotmail.com>
2019-10-30 22:53:37 -0400

    completed work

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const Todo = props => {
+const Todo = ({ toggle, todo }) => {
     return (
-        <div className={`todo{props.todo.completed ? 'completed' : ''}`}
-            onClick={() => props.toggleCompleted(props.todo.id)}>
+        <li
+            onClick={() => toggle(todo.id)}
+            style={
+                todo.completed ? { color: "red", textDecoration: "line-through" } : null
+            }
+        >
+            {todo.task}
+        </li>
+    );
+};
 
-            <p>{props.todo.task}</p>
-
-        </div>
-
-    )
-}
 export default Todo;
-
-
-{/* <span>{props.todo.id}</span> */ }
-{/* <btn className="clear-btn" onClick={props.clearCompleted}>Clear Completed</btn> */ }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,19 +1,99 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
-import React from 'react';
-import Todo from './Todo';
+import React from "react";
+import Todo from "./Todo";
 
-const TodoList = props => {
-    return (
-        <div className="list">
-            {props.todos.map(todo => (
-                <Todo
-                    key={todo.id}
-                    todo={todo}
-                    toggleCompleted={props.toggleCompleted}
-                />
-            ))}
-        </div>
-    )
+class TodoList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            input: "",
+            todos: [
+                {
+                    task: 'Organize Garage',
+                    id: 1528817077286,
+                    completed: false
+                },
+                {
+                    task: 'Bake Cookies',
+                    id: 1528817084358,
+                    completed: false
+
+                }
+            ],
+
+        };
+    }
+
+    upInput = e => {
+        this.setState({
+            input: e.target.value
+        });
+    };
+
+    addTodo = e => {
+        e.preventDefault();
+
+        const id = Date.now();
+
+
+
+        const todo = {
+            task: this.state.input,
+            completed: false,
+            id: id
+        };
+        const todos = [...this.state.todos, todo];
+        this.setState({
+            todos: todos,
+            input: ""
+        });
+    };
+
+    toggleDone = id => {
+        const updatedTodos = this.state.todos.map(todo => {
+            if (todo.id === id) todo.completed = !todo.completed;
+            return todo;
+        });
+        this.setState({ todos: updatedTodos });
+    };
+
+    cleanCompleted = e => {
+        e.preventDefault();
+        const notFinishedTodos = this.state.todos.map(todo => {
+            if (todo.completed === false) return todo;
+        });
+        this.setState({
+            todos: notFinishedTodos
+        });
+    };
+
+    render() {
+        const { input, todos } = this.state;
+
+        return (
+            <div className="App">
+                <div
+                    style={{
+                        marginBottom: "100px",
+                        border: "solid black 1px",
+                        padding: "10%"
+                    }}
+                >
+                    <h1>TodoList</h1>
+                    <input value={input} onChange={this.upInput} />
+                    <button onClick={this.addTodo}>Add Todo</button>
+                    <button onClick={this.cleanCompleted}>Clear Done</button>
+
+                    <ul>
+                        {todos.map((todo, i) => {
+                            return (
+                                <Todo toggle={this.toggleDone} todo={todo} key={todo.id} />
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
 }
+
 export default TodoList;
